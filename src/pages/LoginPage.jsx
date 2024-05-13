d224c14a93cc1195fe5303d3fc23740b23ea9b2e
tofisvej <svejovsky@spaceknow.com>
2024-05-13 15:48:19 +0200

    login done, working on media query

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,22 +1,37 @@
-import React from "react";
-import { useState } from "react";
-import '../styles/LoginPage.scss';
-import 'react-tabs/style/react-tabs.css';
+import React, { useState, useEffect, useRef } from "react";
 import RegisterForm from '../Components/RegisterForm'
 import LoginForm from "../Components/LoginForm";
+import '../styles/LoginPage.scss';
 
 const LoginPage = () => {
   const [activeForm, setActiveForm] = useState('register');
+  const buttonGroupRef = useRef(null);
+
+  useEffect(() => {
+    const updateBorder = () => {
+      const buttons = buttonGroupRef.current.querySelectorAll('button');
+      const activeButton = buttonGroupRef.current.querySelector('.active');
+      if (activeButton) {
+        const borderLeft = activeButton.offsetLeft + 'px';
+        const borderWidth = activeButton.offsetWidth + 'px';
+        buttonGroupRef.current.style.setProperty('--border-left', borderLeft);
+        buttonGroupRef.current.style.setProperty('--border-width', borderWidth);
+      }
+    };
+
+    updateBorder();
+    window.addEventListener('resize', updateBorder); // Adjust on window resize
+    return () => window.removeEventListener('resize', updateBorder);
+  }, [activeForm]);
 
   return (
     <div className="login-page">
       <div className="form-container">
         <div className="text-container">
-            <h1>VÍTEJTE NA STRÁNKÁCH SCALP AND HAIR ACADEMY</h1>
-            <p>Po přihlášení do systému budete přesměrování do galerie videí a fotografií z konference Scalp and Hair Academy 2024</p>
+          <h1>VÍTEJTE NA STRÁNKÁCH SCALP AND HAIR ACADEMY</h1>
+          <p>Po přihlášení do systému budete přesměrování do galerie videí a fotografií z konference Scalp and Hair Academy 2024</p>
         </div>
-
-        <div className="button-group">
+        <div className="button-group" ref={buttonGroupRef}>
           <button 
             className={activeForm === 'register' ? 'active' : ''} 
             onClick={() => setActiveForm('register')}
