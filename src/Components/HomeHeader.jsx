42796c378e2cd0e9a1b47183e0ec40012c55307b
tofisvej <svejovsky@spaceknow.com>
2024-06-27 15:36:06 +0200

    final

--- src/Components/HomeHeader.jsx
+++ src/Components/HomeHeader.jsx
@@ -17,7 +17,8 @@ const HomeHeader = () => {
       <div className="header-info">
         <h1>Informace o přidělování kreditů</h1>
         <Divider component="li" />
-        <p>Vzdělávací akce je pořádána dle Stavovského předpisu ČLK č. 16 pod číslem 116936 a bude ohodnocena 5. KREDITY. Podmínkou přidělení kreditů je zhlédnutí přednášek a uvedení identifikačního čísla lékaře při registraci.</p>
+        <p>Vzdělávací akce je pořádána dle Stavovského předpisu ČLK č. 16 pod číslem 116936 a bude ohodnocena 5. KREDITY. Podmínkou přidělení kreditů je zhlédnutí všech přednášek do 7. září 2024 a uvedení identifikačního čísla lékaře při registraci.
+</p>
       </div>
       <div className="header-profile">
         <img src="/images/monika.jpeg" alt="Prof. MUDr. Monika Arenbergerová, Ph.D." className="profile-image" />
